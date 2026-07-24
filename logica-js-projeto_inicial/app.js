335b9b8421c0eb7debea7907ac977f7e2a28d40a
rafaelagazevedo <rafaelageovanam@gmail.com>
2026-07-24 11:14:13 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('Boas vindas eeeeeao jogo do número secreto');
 let numeroSecreto = 8;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
